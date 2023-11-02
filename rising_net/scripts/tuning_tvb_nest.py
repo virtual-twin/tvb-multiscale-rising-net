86e2a4d3e55be17509bec804c2522b4e42b18560
dionperd <dionperd@gmail.com>
2023-11-02 16:33:57 +0100

    Updated tuning_tvb_nest.py (not tested yet)

--- rising_net/scripts/tuning_tvb_nest.py
+++ rising_net/scripts/tuning_tvb_nest.py
@@ -10,47 +10,53 @@ from tvb_nest_script import *
 from tvb_multiscale.core.plot.plotter import Plotter
 from tvb.contrib.scripts.datatypes.time_series_xarray import TimeSeriesRegion as TimeSeriesXarray
 
-from nest_script import *        #build_NEST_network, plot_nest_results
+from nest_script import *        # build_NEST_network, plot_nest_results
 
-model_params = {'STIMULUS': 0.0, 'G': 6}        # Tuning is done at baseline
 
-tuned_values_tvb_nest = [0.01]    #[0.05]          #[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
+# Assuming:
+# DEFAULT_ARGS = {'G': 6.0, 'STIMULUS': 0.1,
+#                 'I_e': -0.35, 'I_s': 0.085,
+#                 'w_ie': -3.0, 'w_rs': -2.0,
+#                 'CONN_LOG': True, 'FIC': 1.11,  'FIC_SPLIT': 0.31,  #'fit',
+#                 'PRIORS_DIST': 'uniform',
+#                 'output_folder': "", 'verbose': 1, 'plot_flag': True}
 
-#tuned_values_nest_tvb = [100, 150]
+# # TVB - NEST interface parameters:
+# config.TRANSIENT_RATIO =0.25
+# config.MOSSY_MAX_RATE = 122.0  # Hz
+# config.w_TVB_to_NEST = 0.04375
 
-#tuned_value_nest_tvb = 50   # 0.1
-#tuned_value_tvb_nest = 0.65          # 0.8 
+TUNED_VALUES_TVB_TO_NEST = [0.02, 0.04, 0.0425, 0.04375, 0.045, 0.05, 0.075, 0.1]
 
-RMSEs = []
-for tuned_value_tvb_nest in tuned_values_tvb_nest:
+
+def tuning_tvb_nest(w_TVB_to_NEST):
+
+# RMSEs = []
+
+# for w_TVB_to_NEST in TUNED_VALUES_TVB_TO_NEST:
     # Get configuration
-    config, plotter = configure(output_folder='nest_tvb_'+str(tuned_value_tvb_nest)+'_', verbose=2)
-    print("config.NEST_PERIPHERY",config.NEST_PERIPHERY)
-    config.model_params.update(model_params)
-    config.SIMULATION_LENGTH = 3 #20000 30000
+    config, plotter = configure(output_folder='nest_tvb_' + str(w_TVB_to_NEST) + '_', verbose=2,
+                                STIMULUS=0.0)  # We are fitting in resting state!!!
+
+    config.SIMULATION_LENGTH = 30000.0
     config.TRANSIENT_RATIO = 0.25
-    config.MOSSY_MAX_RATE = 100
-    config.w_TVB_to_NEST = tuned_value_tvb_nest
+    config.MOSSY_MAX_RATE = 122.0
+    config.w_TVB_to_NEST = w_TVB_to_NEST
     
     # Load and prepare connectome and connectivity with all possible normalizations:
     connectome, major_structs_labels, voxel_count, inds, maps = prepare_connectome(config, plotter=plotter)
     connectivity = build_connectivity(connectome, inds, config)
-
-    
 
     # Prepare model
     model = build_model(connectivity.number_of_regions, inds, maps, config)
     # Prepare simulator
     simulator = build_simulator(connectivity, model, inds, maps, config, plotter=plotter)
 
-    
     # Build TVB-NEST interfaces
     nest_network, nest_nodes_inds, neuron_models, neuron_number = build_NEST_network(config)
     simulator, nest_network = build_tvb_nest_interfaces(simulator, nest_network, nest_nodes_inds, config)
     # Simulate TVB-NEST model
     results, transient, simulator, nest_network = simulate_tvb_nest(simulator, nest_network, config)
-            
-
 
     # Compute error
     
@@ -59,13 +65,17 @@ for tuned_value_tvb_nest in tuned_values_tvb_nest:
 
     # Compute approximate average rate of mossy fibers as:
     # number_of_spikes / (number_of_neurons * time_length_in_ms) * 1000 (to convert to spikes/sec)
-    duration = config.SIMULATION_LENGTH - transient
-    n_spikes = np.sum(events['times'] > events['times'][-1] - duration)
-    print("Approximate mossy_fibers rate during the last %g ms = %g" % 
-        (duration, n_spikes / 
-                    (nest_network.output_devices['mossy_fibers']['Right Ansiform lobule'].number_of_neurons 
-                    * duration) * 1000))
-    rate = n_spikes / (nest_network.output_devices['mossy_fibers']['Right Ansiform lobule'].number_of_neurons * duration) * 1000
-    RMSEs.append(abs(rate-3.9))
-    
-print("RMSE for TVB-NEST gains ", tuned_values_tvb_nest, " are ", RMSEs)
+    duration = nest_network.nest_instance.GetKernelStatus("biological_time") - transient
+    n_spikes = np.sum(events['times'] > transient)
+    rate = 1000 * n_spikes / \
+           (nest_network.output_devices['mossy_fibers']['Right Ansiform lobule'].number_of_neurons
+            * duration)
+    print("Approximate mossy_fibers rate during the last %g ms = %g" % (duration, rate))
+    RMSE = (rate-3.9)**2
+
+    print("RMSE for TVB-NEST gain = %g is %g" % (w_TVB_to_NEST, RMSE))
+
+    # RMSEs.append(RMSE)
+    # print("RMSE for TVB-NEST gains ", TUNED_VALUES_TVB_TO_NEST, " are ", RMSEs)
+
+    return RMSE
