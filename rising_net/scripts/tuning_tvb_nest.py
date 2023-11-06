828fe788460957b16591e992ddd61a7db9ca8fd7
dionperd <dionperd@gmail.com>
2023-11-06 19:37:49 +0100

    Further beautification in tuning_tvb_nest.py

--- rising_net/scripts/tuning_tvb_nest.py
+++ rising_net/scripts/tuning_tvb_nest.py
@@ -26,10 +26,11 @@ from nest_script import *        # build_NEST_network, plot_nest_results
 # config.MOSSY_MAX_RATE = 122.0  # Hz
 # config.w_TVB_to_NEST = 0.04375
 
+SIMULATION_LENGTH = 30000.0
 TUNED_VALUES_TVB_TO_NEST = [0.02, 0.04, 0.0425, 0.04375, 0.045, 0.05, 0.075, 0.1]
 
 
-def tuning_tvb_nest(w_TVB_to_NEST):
+def tuning_tvb_nest(w_TVB_to_NEST=0.04375, **kwargs):
 
 # RMSEs = []
 
@@ -38,11 +39,11 @@ def tuning_tvb_nest(w_TVB_to_NEST):
     config, plotter = configure(output_folder='nest_tvb_' + str(w_TVB_to_NEST) + '_', verbose=2,
                                 STIMULUS=0.0)  # We are fitting in resting state!!!
 
-    config.SIMULATION_LENGTH = 30000.0
-    config.TRANSIENT_RATIO = 0.25
-    config.MOSSY_MAX_RATE = 122.0
     config.w_TVB_to_NEST = w_TVB_to_NEST
-    
+    config.MOSSY_MAX_RATE = kwargs.get("mossy_max_rate", config.MOSSY_MAX_RATE)     # 122.0 Hz
+    config.SIMULATION_LENGTH = kwargs.get("simulation_length", SIMULATION_LENGTH)
+    config.TRANSIENT_RATIO = kwargs.get("transient_ratio", config.TRANSIENT_RATIO)  # 0.25
+
     # Load and prepare connectome and connectivity with all possible normalizations:
     connectome, major_structs_labels, voxel_count, inds, maps = prepare_connectome(config, plotter=plotter)
     connectivity = build_connectivity(connectome, inds, config)
@@ -73,7 +74,7 @@ def tuning_tvb_nest(w_TVB_to_NEST):
     print("Approximate mossy_fibers rate during the last %g ms = %g" % (duration, rate))
     RMSE = (rate-3.9)**2
 
-    print("RMSE for TVB-NEST gain = %g is %g" % (w_TVB_to_NEST, RMSE))
+    print("RMSE for TVB-NEST gain = %g is %g" % (config.w_TVB_to_NEST, RMSE))
 
     # RMSEs.append(RMSE)
     # print("RMSE for TVB-NEST gains ", TUNED_VALUES_TVB_TO_NEST, " are ", RMSEs)
