a9af27a3ac40e033f54e7bb15e25b75493a8393c
dionperd <dionperd@gmail.com>
2024-01-18 18:36:48 +0100

    Warning for when SBI/torch cannot be imported in scripts.py

--- rising_net/scripts/scripts.py
+++ rising_net/scripts/scripts.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import warnings
 from rising_net.scripts.base import *
 from rising_net.scripts.tvb_script import *
 from rising_net.scripts.nest_script import *
-from rising_net.scripts.sbi_script import *
+try:
+    from rising_net.scripts.sbi_script import *
+except Exception as e:
+    warnings.warn(str(e))
 from rising_net.scripts.tvb_nest_script import *
