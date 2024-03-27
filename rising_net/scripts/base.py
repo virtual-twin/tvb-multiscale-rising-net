13f468fe84d42c7c997af9c40d9b72fe6c4ff5b2
dionperd <dionperd@gmail.com>
2024-03-27 19:36:16 +0100

    WHISKERS, tau_w and I_s & gains defaults changes in base.py

--- rising_net/scripts/base.py
+++ rising_net/scripts/base.py
@@ -21,19 +21,21 @@ from tvb.simulator.integrators import EulerStochastic
 
 
 DEFAULT_ARGS = {# TVB model:
-                'I_s': 0.085,
+                'I_s': 0.1,  # 0.085,
                 "STIMULUS": 0.25,
+                "WHISKERS": 0,
+                "tau_w": 10.0,
                 # TVB network:
                 'G': 6.0,
                 'FIC': 1.11,  # 2.0,
                 'FIC_SPLIT': 0.31,  # 2.0,
                 # Pathway gains:
                 "PATHWAY_GAIN": 1,
-                "TRIG_GAIN": 50.0, "MEDULLA_GAIN": 50.0, "CEREB_GAIN": 50.0,
-                "TRIGS1_GAIN": 15.0, "MEDULLAS1_GAIN": 15.0, "CNS1_GAIN": 20.0,
+                "TRIG_GAIN": 100.0, "MEDULLA_GAIN": 50.0, "CEREB_GAIN": 50.0,
+                "TRIGS1_GAIN": 10.0, "MEDULLAS1_GAIN": 10.0, "CNS1_GAIN": 30.0,
                 "CNM1_GAIN": 50.0,
-                "M1S1_GAIN": 50.0,
-                "M1FACIAL_GAIN": 1.0,   # 50.0,
+                "M1S1_GAIN": 10.0,
+                "M1FACIAL_GAIN": 50.0,   # 50.0,
                 "FACIALTRIG_GAIN": 1.0,  # 50.0,
                 # TVB <-> NEST Interface:
                 "w_TVB_to_NEST": 35.0, "w_TVB_to_NEST_rest": 0.15,
@@ -130,6 +132,7 @@ def configure(**ARGS):
     config.DEFAULT_INTEGRATOR = config.DEFAULT_STOCHASTIC_INTEGRATOR
 
     # Connectivity
+    config.WHISKERS = args.get("WHISKERS", 0)
     config.CONN_SPEED = 3.0
     config.BRAIN_CONN_FILE = tvb_conn_filepath
     config.MAJOR_STRUCTS_LABELS_FILE = major_structs_labels_filepath
@@ -168,6 +171,7 @@ def configure(**ARGS):
     config.model_params['I_e'] = args.get('I_e', -0.35)
     config.model_params['w_ie'] = args.get('w_ie', -3.0)
     config.model_params['w_rs'] = args.get('w_rs', -2.0)
+    config.model_params['tau_w'] = args.get('tau_w', 10.0)
     config.model_params['STIMULUS'] = args.get('STIMULUS', 0.25)  # 0.25
     config.STIMULUS_RATE = 8.0  # Hz
     config.STIMULUS_BASELINE = args.get('STIMULUS_BASELINE', 1.0)  # 1.0 or 0.0
