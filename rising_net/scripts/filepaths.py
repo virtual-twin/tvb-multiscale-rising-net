f266a759f23c8c3a0d6bd33305f4b8723b55440c
dionperd <dionperd@gmail.com>
2024-08-19 19:03:12 +0300

    Added filepaths.py

---
+++ rising_net/scripts/filepaths.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+def get_path(config, folder="", mode=""):
+    if mode == "figs":
+        base_path, mode = os.path.split(config.figures.FOLDER_FIGURES)
+    elif mode == "res":
+        base_path, mode = os.path.split(config.out.FOLDER_RES)
+    else:
+        base_path, _ = os.path.split(config.out.FOLDER_RES)
+    return os.path.join(base_path, folder, mode)
+
+
+def get_res_path(config, folder=""):
+    return get_path(config, folder, mode="res")
+
+
+def get_fig_path(config, folder=""):
+    return get_path(config, folder, mode="figs")
+
+
+def filepath_prefixes(filepath, iR=None, label=""):
+    if len(label):
+        filepath += "_%s" % label
+    if iR is not None:
+        filepath += iRstr(iR)
+    return filepath
+
+
+def iRstr(iR, n=2):
+    format = "_%0" + "%d" % n + "d"
+    return format % iR
+
+
+def construct_filepath(default_filename, original_path, iR=None, label="", filepath=None, extension=None):
+    if filepath is None or extension is None:
+        filepath, extension = os.path.splitext(os.path.join(original_path, default_filename))
+    filepath = filepath_prefixes(filepath, iR, label)
+    return "%s%s" % (filepath, extension)
+
+
+def simres_filepath(config, default_filename="", folder="", iR=None, label="", filepath=None, extension=None):
+    if len(default_filename) == 0:
+        default_filename = config.SIM_RES_FILE
+    return construct_filepath(default_filename,
+                              get_res_path(config, folder),
+                              iR=iR, label=label,
+                              filepath=filepath, extension=extension)
+
+
+def figs_filepath(config, default_filename, folder="", iR=None, label="", filepath=None, extension=None):
+    return construct_filepath(default_filename,
+                              get_fig_path(config, folder),
+                              iR=iR, label=label,
+                              filepath=filepath, extension=extension)
