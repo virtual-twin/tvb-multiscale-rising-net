a39e1ca79622ab76280047b03e7b8613e72d3c3b
dionperd <dionperd@gmail.com>
2024-09-26 15:37:44 +0200

    Added diagnostics & stats plots, corrections & improvements in sbi_script.py

--- rising_net/scripts/sbi_script.py
+++ rising_net/scripts/sbi_script.py
@@ -6,9 +6,11 @@ import warnings
 import pickle
 from copy import deepcopy
 
+import matplotlib.pyplot as plt
 import numpy
 import numpy as np
-from xarray import DataArray
+from pandas import Index
+from xarray import DataArray, concat
 import torch
 from sbi.inference.base import infer, prepare_for_sbi, simulate_for_sbi
 from sbi.inference import SNPE
@@ -71,16 +73,16 @@ def posterior_filepath(config, iR=None, label="", filepath=None, extension=None)
                            filepath=filepath, extension=extension)
 
 
-def posterior_samples_filepath(config, iR=None, label="", filepath=None, extension=None):
+def posterior_samples_filepath(config, label="", filepath=None, extension=None):
     return samples_filepath(config, config.SAMPLES_FILE,
                             folder=config.FIT_FOLDER,
-                            iR=iR, label=label, filepath=filepath, extension=extension)
+                            label=label, filepath=filepath, extension=extension)
 
 
-def PPC_samples_filepath(config, iR=None, label="", filepath=None, extension=None):
+def PPC_samples_filepath(config, label="", filepath=None, extension=None):
     return samples_filepath(config, config.SAMPLES_FILE,
                             folder=config.PPC_FOLDER,
-                            iR=iR, label=label, filepath=filepath, extension=extension)
+                            label=label, filepath=filepath, extension=extension)
 
 
 def params_pairplot(samples, points=None, metric=None, config=None, figname=None, figpath=None):
@@ -224,7 +226,7 @@ def write_posterior_samples(results_i, config,
                             iR=None, label="",
                             results=None, save_samples=True):
     config = assert_config(config, return_plotter=False)
-    filepath = posterior_samples_filepath(config, iR=None, label=label)  # Write all runs to the same file
+    filepath = posterior_samples_filepath(config, label=label)  # Write all runs to the same file
     if results is None:
         if os.path.isfile(filepath):
             with open(filepath, "rb") as handle:
@@ -250,9 +252,9 @@ def load_posterior(iR=None, label="", config=None):
     return posterior
 
 
-def load_posterior_samples(iR=None, label="", config=None):
+def load_posterior_samples(label="", config=None):
     config = assert_config(config, return_plotter=False)
-    filepath = posterior_samples_filepath(config, iR, label)
+    filepath = posterior_samples_filepath(config, label)
     with open(filepath, "rb") as handle:
         results = pickle.load(handle)
     return results
@@ -273,21 +275,6 @@ def add_posterior_samples_iR(all_samples, samples_iR):
                     vals.append(vl)
             all_samples[key].append(vals)
     return all_samples
-
-
-def load_posterior_samples_all_runs(runs=None, label="", samples=None, config=None):
-    config = assert_config(config, return_plotter=False)
-    if samples is None:
-        samples = OrderedDict()
-    if runs is None:
-        runs = list(range(config.N_FIT_RUNS))
-    for iR in runs:
-        try:
-            samples_iR = load_posterior_samples(iR, label, config)
-            samples = add_posterior_samples_iR(samples, samples_iR)
-        except Exception as e:
-            warnings.warn("Failed to load posterior samples for run iR=%d!\n%s" % (iR, str(e)))
-    return samples
 
 
 def sbi_train(priors, train_params_samples, sim_res, verbosity):
@@ -330,14 +317,12 @@ def train_posterior(train_params_samples, measures, priors=None,
     if plot_flag:
         fig, axes = \
             plot_training_params_samples(train_params_samples, label=label, config=config)
-
     if priors is None:
         priors = build_priors(config)
     posterior = sbi_train(priors,
                           torch.Tensor(train_params_samples),
                           torch.Tensor(measures),
                           verbosity)
-
     return posterior
 
 
@@ -416,6 +401,56 @@ def sbi_infer(priors, train_params_samples, sim_res, n_samples_per_run, target, 
                         target, n_samples_per_run, verbosity)
 
 
+def get_diagnostic(diagnostic, samples, config, params=None, iR=None):
+    if params is None:
+        params = config.PRIORS_PARAMS_NAMES
+    if iR is None:
+        iRs = slice(None)
+    else:
+        iRs = iR
+    res = np.vstack(samples[diagnostic][iRs])
+    name = diagnostic
+    coords = {"Parameter": params}
+    if iR is None and res.shape[0] > 1:
+        iR = np.arange(res.shape[0]).astype("i")
+    if iR is not None:
+        dims=["Fitting repetition", "Parameter"]
+        coords[dims[0]] = iR
+    else:
+        dims = ["Parameter"]
+        res = res.squeeze()
+        name = "%s all samples fitting" % diagnostic
+    return DataArray(res, name=name, dims=dims, coords=coords)
+
+
+def plot_diagnostics(samples, config,
+                     diagnostics=["mean", "std", "diff", "accuracy", "zscore_prior", "zscore", "shrinkage"],
+                     params=None, iR=None, label="", figsize=None, figname=None, figpath=None):
+
+    res = []
+    dnames = []
+    for d in diagnostics:
+        if d in samples:
+            res.append(get_diagnostic(d, samples, config, params=params, iR=iR))
+            dnames.append(d)
+    res = concat(res, dim=Index(dnames, name="Inference diagnostics"))
+
+    if figsize is None:
+        figsize = config.figures.DEFAULT_SIZE
+
+    if res.ndim > 2:
+        res.plot.line(x=res.dims[1], row=res.dims[0], hue=res.dims[2],
+                      marker="*", markersize=10, figsize=figsize)
+    else:
+        res.to_series().unstack().plot.bar(figsize=figsize)
+    if config.figures.SAVE_FLAG:
+        if figpath is None:
+            if figname is None:
+                figname = 'diagnostics' + "." + config.figures.FIG_FORMAT
+        figpath = fitfigs_filepath(config, figname, iR=iR, label=label)
+        plt.savefig(figpath)
+
+
 def plot_infer(samples=None, results=None, points=None, metric=None, iR=None, label="", config=None):
     config = assert_config(config, return_plotter=False)
     if iR is None:
@@ -438,12 +473,14 @@ def plot_infer(samples=None, results=None, points=None, metric=None, iR=None, la
         metric = None
         points = None
     if config.figures.SAVE_FLAG:
-        figname = 'posterior_samples_pairplot'
-        figname += "." + config.figures.FIG_FORMAT
+        figname = 'posterior_samples_pairplot' + "." + config.figures.FIG_FORMAT
         figpath = fitfigs_filepath(config, figname, iR=iR, label=label)
     else:
         figpath=None
-    return params_pairplot(samples, points=points, metric=metric, config=config, figpath=figpath)
+    fig, axes = params_pairplot(samples, points=points, metric=metric, config=config, figpath=figpath)
+    if len(config.FIT_DIAGNOSTICS):
+        plot_diagnostics(samples, config, diagnostics=config.FIT_DIAGNOSTICS, params=None, iR=iR,  label=label)
+    return fig, axes
 
 
 def infer_workflow(train_params_samples, sim_res, priors=None, target=None, ground_truth=None, config=None,
@@ -496,7 +533,7 @@ def infer_nRuns(train_params_samples, sim_res, priors=None, target=None, groung_
         for iR in range(config.N_FIT_RUNS):
             if verbosity:
                 print("\n\nFitting run %d / %d with %d training samples!..\n" %
-                      (iR, config.N_FIT_RUNS, n_train_samples))
+                      (iR+1, config.N_FIT_RUNS, n_train_samples))
             ticR = time.time()
             # Choose a subsample of the whole set of samples:
             sampl_inds = random.sample(all_inds, n_train_samples)
@@ -516,13 +553,73 @@ def infer_nRuns(train_params_samples, sim_res, priors=None, target=None, groung_
         if verbosity:
             print("Plotting samples from all %d runs together..." % config.N_FIT_RUNS)
         plot_infer(samples=None, results=results_i, points=None, metric="MAP",
-                   iR=None, label=joinstr([label, "allruns"]), config=config)
+                   iR=None, label=joinstr([label, config.ALL_RUNS_LABEL]), config=config)
     if verbosity:
         print("\n\nFitting with all samples!..\n")
     results = infer_workflow(train_params_samples, sim_res, priors=priors,
                              target=target, ground_truth=groung_truth, config=config,
-                             label=joinstr([label, "allsamples"]),
+                             label=joinstr([label, config.ALL_SAMPLES_LABEL]),
                              n_samples_per_run=n_samples_per_run, measure_labels=measure_labels,
                              results=None, iR=None, save_samples=save_samples,
                              plot_flag=plot_flag, verbosity=verbosity)[1]
     return results, results_i
+
+
+def plot_stats(measures, stat="PPC", target=None, params=None, label="", measure_labels=None, config=None):
+    config = assert_config(config, return_plotter=False, plot_flag=False)
+    fig1, axes1 = plot_samples_measures_and_targets(measures, target=target,
+                                                    label="%s_%s" % (stat, label),
+                                                    measure_labels=measure_labels, config=config)
+    if params is not None:
+        fig2, axes2 = params_pairplot(params, points=params.mean(axis=0), metric="mean", config=config,
+                                      figpath=fitfigs_filepath(config, "params_pairplot.png",
+                                                               label="%s_%s" % (stat, label)))
+        return fig1, axes1, fig2, axes2
+    return fig1, axes1
+
+
+def correlation_distance(data, target):
+    return 1 - np.corrcoef(data, target)[:, -1][:-1]
+
+
+def get_best_stat_sims_params_target(measures, target, params=None, label="",
+                                     target_dist_fun=correlation_distance, Nbest=None):
+    if measures.ndim < 2:
+        measures = measures[np.newaxis]
+    Nm = measures.shape[0]
+    if params is not None:
+        if params.ndim < 2:
+            params = params[np.newaxis]
+        Nparams = params.shape[0]
+        if Nparams != Nm:
+            raise ValueError("The number of parameters Nparams = %d "
+                             "is not equal to the number of measures Nm = %d!" % (Nparams, Nm))
+
+    if Nm == 1:
+        if Nbest is not None and Nbest > Nm:
+            warnings.warn("\nThe number of measures (Nm=%d) is smaller "
+                          "than the required number of best measures (Nbest=%d)!\n"
+                          "Setting Nbest=Nm=%d and using all measures!\n" % (Nm, Nbest, Nm))
+        return measures, params, joinstr(["best", label])
+    else:
+        if Nbest is None:
+            Nbest = np.maximum(int(np.ceil(Nm / 2)), 1)  # In this case we set safely Nbest as Nm/2
+        elif Nbest > Nm:
+            warnings.warn("\nThe number of measures (Nm=%d) is smaller "
+                          "than the required number of best measures (Nbest=%d)!\n"
+                          "Setting Nbest=Nm=%d and using all measures!\n" % (Nm, Nbest, Nm))
+            return measures, params, joinstr(["all_%d_best" % Nm, label])
+        if target.ndim < 2:
+            target = target[np.newaxis]
+        target_dist_best_sorted_inds = np.argsort(target_dist_fun(measures, target))[:Nbest]
+        if params is not None:
+            params = params[target_dist_best_sorted_inds]
+        return measures[target_dist_best_sorted_inds], params, joinstr(["best_%d" % Nbest, label])
+
+
+def plot_best_stat_sims_params_target(measures, target, stat="PPC", params=None, label="",
+                                      target_dist_fun=correlation_distance, Nbest=None,
+                                      measure_labels=None, config=None):
+    config = assert_config(config, return_plotter=False)
+    measures, params, label = get_best_stat_sims_params_target(measures, target, params, label, target_dist_fun, Nbest)
+    return plot_stats(measures, stat, target, params, label, measure_labels, config)
