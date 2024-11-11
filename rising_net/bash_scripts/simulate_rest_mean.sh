c8335e3d092f944b2a3e85d090b87e9a790e5a2b
dionperd <dionperd@gmail.com>
2024-11-11 19:48:35 +0100

    Added bash script files for simulations

---
+++ rising_net/bash_scripts/simulate_rest_mean.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CMD="python"
+CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
+CMD_SCRIPT="rest_run_fit_plot.py"
+DEF_ARGS="--FUNCMODE MEANSIM --MODE TVB --BASENAME FIT_REST --fitlabel allsamples"
+
+while getopts p:s:a: flag
+do
+    case "${flag}" in
+        p) CMD_PATH=${OPTARG};;
+        s) CMD_SCRIPT=${OPTARG};;
+        a) ARGS=${OPTARG};;
+    esac
+done
+
+echo ${ARGS}
+CMD_ARGS="${DEF_ARGS} ${ARGS}"
+echo ${CMD_ARGS}
+CMD="$CMD $CMD_PATH/$CMD_SCRIPT ${CMD_ARGS}"
+
+echo ${CMD}
+${CMD}
---
+++ rising_net/bash_scripts/simulate_task_mean.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CMD="python"
+CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
+CMD_SCRIPT="task_run_fit_plot.py"
+DEF_ARGS="--FUNCMODE MEANSIM --BASENAME FIT_TASK --fitlabel allsamples"
+
+while getopts p:s:a: flag
+do
+    case "${flag}" in
+        p) CMD_PATH=${OPTARG};;
+        s) CMD_SCRIPT=${OPTARG};;
+        a) ARGS=${OPTARG};;
+    esac
+done
+
+echo ${ARGS}
+CMD_ARGS1="${DEF_ARGS} --MODE TVB ${ARGS}"
+echo ${CMD_ARGS1}
+CMD_ARGS2="${DEF_ARGS} --MODE TVB_CEREBOFF ${ARGS}"
+echo ${CMD_ARGS2}
+
+CMD1="$CMD $CMD_PATH/$CMD_SCRIPT ${CMD_ARGS1}"
+echo ${CMD1}
+CMD2="$CMD $CMD_PATH/$CMD_SCRIPT ${CMD_ARGS2}"
+echo ${CMD2}
+
+${CMD1}
+${CMD2}
