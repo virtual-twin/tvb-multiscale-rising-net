9067a9db9d4b7ba0fee0ce2811869101ab3e61ae
dionperd <dionperd@gmail.com>
2024-11-15 12:30:14 +0100

    Added fit_rest_jobarr.sh and sbatch_job_array_rest_fit.sh

---
+++ rising_net/bash_scripts/fit_rest_jobarr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+JOBARRID=$1
+CMD="python"
+CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
+CMD_SCRIPT="bash_utils.py"
+PYTHON_CMD="fit_rest"
+
+$DOCKER_PYTHONPATH_CMD
+
+CMD="$CMD $CMD_PATH/$CMD_SCRIPT $PYTHON_CMD $JOBARRID"
+echo ${CMD}
+
+# ${CMD}
