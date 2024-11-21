b60c9619e71b79913cd9e63e36f12c273e88c2b0
dionperd <dionperd@gmail.com>
2024-11-21 16:13:14 +0100

    Added simulate_rest/task_ids_args in bash_utils.py

--- rising_net/scripts/bash_utils.py
+++ rising_net/scripts/bash_utils.py
@@ -6,6 +6,11 @@ def jobarr_id_to_task_ids(args):
     return np.unravel_index(args[0], args[1:], order='C')
 
 
+def simulate_rest_ids_args(jobarr_id, Ngs=11, Nreps=3):
+    output = print("--iG %d --iR %d" % jobarr_id_to_task_ids([int(jobarr_id), int(Ngs), int(Nreps)]))
+    return output
+
+
 def simulate_rest_train_ids_args(jobarr_id, Nps=1000, Ngs=11, Nreps=3):
     output = print("--iP %d --iG %d --iR %d" % jobarr_id_to_task_ids([int(jobarr_id), int(Nps), int(Ngs), int(Nreps)]))
     return output
@@ -24,6 +29,11 @@ def simulate_rest_mapmean_ids_args(jobarr_id, Ngs=10, Nreps=3):
     iG, iR = jobarr_id_to_task_ids([int(jobarr_id), int(Ngs), int(Nreps)])
     iG = iG + 1
     output = print("--iG %d --iR %d" % (iG, iR))
+    return output
+
+
+def simulate_task_ids_args(jobarr_id, Nreps=3):
+    output = print("--iR %d" % jobarr_id_to_task_ids([int(jobarr_id), int(Nreps)]))
     return output
 
 
@@ -84,6 +94,8 @@ if __name__ == '__main__':
             condition(x)
     elif sys.argv[1] == "fit_rest":
         fit_rest(*sys.argv[2:])
+    elif sys.argv[1] == "simulate_rest_ids_args":
+        simulate_rest_ids_args(*sys.argv[2:])
     elif sys.argv[1] == "simulate_rest_train_ids_args":
         simulate_rest_train_ids_args(*sys.argv[2:])
     elif sys.argv[1] == "simulate_rest_ppc_ids_args":
@@ -92,6 +104,8 @@ if __name__ == '__main__':
         simulate_rest_mapmean_ids_args(*sys.argv[2:])
     # elif sys.argv[1] == "fit_task":
     #     fit_task(*sys.argv[2:])
+    elif sys.argv[1] == "simulate_task_ids_args":
+        simulate_task_ids_args(*sys.argv[2:])
     elif sys.argv[1] == "simulate_task_train_ids_args":
         simulate_task_train_ids_args(*sys.argv[2:])
     elif sys.argv[1] == "simulate_task_ppc_ids_args":
