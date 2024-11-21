74a99e1b3bf34bd4eb7f8266aaeaeb3764d44a12
dionperd <dionperd@gmail.com>
2024-11-21 11:04:45 +0100

    Added sparse sampbling of PPC samples per 10 in simulate_rest_ppc_ids_args in bash_utils.py

--- rising_net/scripts/bash_utils.py
+++ rising_net/scripts/bash_utils.py
@@ -14,7 +14,9 @@ def simulate_rest_train_ids_args(jobarr_id, Nps=1000, Ngs=11, Nreps=3):
 def simulate_rest_ppc_ids_args(jobarr_id, Nps=100, Ngs=10, Nreps=3):
     # TODO: Correct this if iG = 0 comes back!
     iP, iG, iR = jobarr_id_to_task_ids([int(jobarr_id), int(Nps), int(Ngs), int(Nreps)])
-    output = print("--iP %d --iG %d --iR %d" % (iP, iG+1, iR))
+    iG = iG + 1
+    iP = iP*10
+    output = print("--iP %d --iG %d --iR %d" % (iP, iG, iR))
     return output
 
 
