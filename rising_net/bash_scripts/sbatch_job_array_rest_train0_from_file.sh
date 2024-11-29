587f2655b56e7194218ca8404a7cc147651168f3
dionperd <dionperd@gmail.com>
2024-11-29 11:58:15 +0100

    Added sbatch files for training reading JOB array ID args from file

---
+++ rising_net/bash_scripts/sbatch_job_array_rest_train0_from_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=FIT_REST0
+#SBATCH --output=./outputs/FIT_REST/logs/FIT_REST0_%A_%a.out
+#SBATCH --error=./outputs/FIT_REST/errors/FIT_REST0_%A_%a.err
+#SBATCH --array=0-5499%100
+#SBATCH --time=30:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --mail-user=dionysios.perdikis@bih-charite.de
+
+JOB_ID=$((${SLURM_ARRAY_TASK_ID} + 0*5500))
+CMD_PATH="/home/docker/packages/tvb-multiscale/rising_net/bash_scripts/"
+CMD="sh ${CMD_PATH}simulate_rest_train_jobarr_from_file.sh ${JOB_ID}"
+apptainer exec --bind $SCRATCH:$DOCKER_SCRATCH,$RISING_NET:$DOCKER_MULTISCALE --pwd $DOCKER_SCRATCH $RISING_NET_IMAGE $CMD
