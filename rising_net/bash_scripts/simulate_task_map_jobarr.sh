b68ba52ebac69af7f57469240a5f1707a44c9822
dionperd <dionperd@gmail.com>
2024-12-12 22:41:40 +0100

    Modified the simulate_task*.sh scripts

--- rising_net/bash_scripts/simulate_task_map_jobarr.sh
+++ rising_net/bash_scripts/simulate_task_map_jobarr.sh
@@ -4,27 +4,28 @@ JOBARRID=$1
 CMD="python"
 CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
 CMD_SCRIPT="task_run_fit_plot.py"
-CMD_SCRIPTID="bash_utils.py"
-CMDID="simulate_task_mapmean_ids_args"
-DEF_ARGS="--FUNCMODE MAPSIM --BASENAME FIT_TASK --fitlabel allsamples"
+BASIC_ARGS="--FUNCMODE MAPSIM --BASENAME FIT_TASK --fitlabel allsamples --iG $JOBARRID"
+DEF_ARGS="$BASIC_ARGS --REST_BASENAME FIT_REST --restfitlabel allsamples"
 
 $DOCKER_PYTHONPATH_CMD
 
-CMDIDS="$CMD $CMD_PATH/$CMD_SCRIPTID $CMDID $JOBARRID"
-echo ${CMDIDS}
+ARGS1="${DEF_ARGS} --MODE TVB"
+echo ${ARGS1}
+ARGS2="${DEF_ARGS} --MODE TVB_CEREBOFF"
+echo ${ARGS2}
 
-ARGSIDS=$($CMDIDS)
-echo ${ARGSIDS}
+for iR in {0..2}; do
 
-CMD_ARGS1="${DEF_ARGS} ${ARGSIDS} --MODE TVB"
-echo ${CMD_ARGS1}
-CMD_ARGS2="${DEF_ARGS} ${ARGSIDS} --MODE TVB_CEREBOFF"
-echo ${CMD_ARGS2}
+  CMD1="$CMD $CMD_PATH/$CMD_SCRIPT ${ARGS1} --iR $iR"
+  echo ${CMD1}
+  ${CMD1}
 
-CMD1="$CMD $CMD_PATH/$CMD_SCRIPT ${CMD_ARGS1}"
-echo ${CMD1}
-${CMD1}
+  CMD2="$CMD $CMD_PATH/$CMD_SCRIPT ${ARGS2} --iR $iR"
+  echo ${CMD2}
+  ${CMD2}
 
-CMD2="$CMD $CMD_PATH/$CMD_SCRIPT ${CMD_ARGS2}"
-echo ${CMD2}
-${CMD2}
+done
+
+PLOTCMD="$CMD $CMD_PATH/$CMD_SCRIPT --function load_and_plot_comparisons $BASIC_ARGS"
+echo $PLOTCMD
+$PLOTCMD
--- rising_net/bash_scripts/simulate_task_ppc_jobarr.sh
+++ rising_net/bash_scripts/simulate_task_ppc_jobarr.sh
@@ -5,8 +5,9 @@ CMD="python"
 CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
 CMD_SCRIPT="task_run_fit_plot.py"
 CMD_SCRIPTID="bash_utils.py"
-CMDID="simulate_task_ppc_ids_args"
-DEF_ARGS="--FUNCMODE PPCSIM --BASENAME FIT_TASK --fitlabel allsamples"
+CMDID="simulate_task_train_ids_args"
+BASIC_ARGS="--FUNCMODE PPCSIM --BASENAME FIT_TASK --fitlabel allsamples"
+DEF_ARGS="$BASIC_ARGS --REST_BASENAME FIT_REST --restfitlabel allsamples"
 
 $DOCKER_PYTHONPATH_CMD
 
@@ -21,10 +22,18 @@ echo ${CMD_ARGS1}
 CMD_ARGS2="${DEF_ARGS} ${ARGSIDS} --MODE TVB_CEREBOFF"
 echo ${CMD_ARGS2}
 
-CMD1="$CMD $CMD_PATH/$CMD_SCRIPT ${CMD_ARGS1}"
-echo ${CMD1}
-${CMD1}
+for iR in {0..2}; do
 
-CMD2="$CMD $CMD_PATH/$CMD_SCRIPT ${CMD_ARGS2}"
-echo ${CMD2}
-${CMD2}
+  CMD1="$CMD $CMD_PATH/$CMD_SCRIPT ${CMD_ARGS1} --iR $iR"
+  echo ${CMD1}
+  ${CMD1}
+
+  CMD2="$CMD $CMD_PATH/$CMD_SCRIPT ${CMD_ARGS2} --iR $iR"
+  echo ${CMD2}
+  ${CMD2}
+
+done
+
+PLOTCMD="$CMD $CMD_PATH/$CMD_SCRIPT --function load_and_plot_comparisons $BASIC_ARGS $ARGSIDS"
+echo $PLOTCMD
+$PLOTCMD
