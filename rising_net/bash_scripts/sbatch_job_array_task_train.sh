60fdf29cf3f6500cfde81ba0e8935ff209af61e0
dionperd <dionperd@gmail.com>
2024-12-17 14:57:55 +0100

    Added sbatch_job_array_task_train.sh

---
+++ rising_net/bash_scripts/sbatch_job_array_task_train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=TASK
+#SBATCH --output=./outputs/TASK/logs/TASK_%A_%a.out
+#SBATCH --error=./outputs/TASK/errors/TASK_%A_%a.err
+#SBATCH --array=4000-8999
+#SBATCH --time=06:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --mail-user=dionysios.perdikis@bih-charite.de
+
+JOB_ID=$((${SLURM_ARRAY_TASK_ID}))
+CMD_PATH="/home/docker/packages/tvb-multiscale/rising_net/bash_scripts/"
+CMD="bash ${CMD_PATH}simulate_task_train_jobarr.sh ${JOB_ID}"
+apptainer exec --bind $SCRATCH:$DOCKER_SCRATCH,$RISING_NET:$DOCKER_MULTISCALE --pwd $DOCKER_SCRATCH $RISING_NET_IMAGE $CMD
