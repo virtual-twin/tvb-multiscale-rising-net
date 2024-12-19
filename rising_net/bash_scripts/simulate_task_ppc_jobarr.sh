ec73841cf4bd8d9f6cbf3e858c1c39fa511f88ed
dionperd <dionperd@gmail.com>
2024-12-19 14:53:36 +0100

    Corrected simulate_task_ppc_jobarr.sh

--- rising_net/bash_scripts/simulate_task_ppc_jobarr.sh
+++ rising_net/bash_scripts/simulate_task_ppc_jobarr.sh
@@ -5,7 +5,7 @@ CMD="python"
 CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
 CMD_SCRIPT="task_run_fit_plot.py"
 CMD_SCRIPTID="bash_utils.py"
-CMDID="simulate_task_train_ids_args"
+CMDID="simulate_task_ppc_ids_args"
 BASIC_ARGS="--FUNCMODE PPCSIM --BASENAME FIT_TASK --fitlabel allsamples"
 DEF_ARGS="$BASIC_ARGS --REST_BASENAME FIT_REST --restfitlabel allsamples"
 
