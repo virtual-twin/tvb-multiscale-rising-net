3db487fdfc6c7a50c5be0d99a46aa85be0e265f5
dionperd <dionperd@gmail.com>
2025-01-06 17:47:31 +0100

    Added bash_scripts for simulating specific inds from file for TASK training

---
+++ rising_net/bash_scripts/simulate_task_train_jobarr_from_file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+JOBARRID=$1
+CMD="python"
+CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
+CMD_SCRIPT="task_run_fit_plot.py"
+CMD_SCRIPTID_PATH='/home/docker/scratch'
+CMD_SCRIPTID_FILE="jobarray.txt"
+BASIC_ARGS="--FUNCMODE TRAINSIM --BASENAME FIT_TASK"
+DEF_ARGS="$BASIC_ARGS --REST_BASENAME FIT_REST --restfitlabel allsamples"
+
+$DOCKER_PYTHONPATH_CMD
+
+ARGSIDS=$(sed -n $(($JOBARRID))p $CMD_SCRIPTID_PATH/$CMD_SCRIPTID_FILE)
+echo ${ARGSIDS}
+
+CMD_ARGS="${DEF_ARGS} ${BASIC_ARGS}  ${ARGSIDS}"
+echo ${CMD_ARGS}
+
+CMD="$CMD $CMD_PATH/$CMD_SCRIPT ${CMD_ARGS}"
+echo ${CMD}
+${CMD}
