a6202f743dac784bf2881c05fc4542b4682941be
dionperd <dionperd@gmail.com>
2025-01-07 16:07:37 +0100

    Adjusted job array in sbatch_job_array_task_train.sh

--- rising_net/bash_scripts/sbatch_job_array_task_train.sh
+++ rising_net/bash_scripts/sbatch_job_array_task_train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name=FTtrain
-#SBATCH --output=./outputs/FIT_TASK/logs/FIT_TASK_%A_%a.out
-#SBATCH --error=./outputs/FIT_TASK/errors/FIT_TASK_%A_%a.err
-#SBATCH --array=4000-8999
+#SBATCH --output=./outputs/FIT_TASK/train_sims/logs/FIT_TASK_%A_%a.out
+#SBATCH --error=./outputs/FIT_TASK/train_sims/errors/FIT_TASK_%A_%a.err
+#SBATCH --array=0-9999
 #SBATCH --time=06:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
