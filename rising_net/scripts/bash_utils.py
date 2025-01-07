af348f0232b34ff7b034b1017d0d85b9fc7e7f13
dionperd <dionperd@gmail.com>
2025-01-07 12:36:49 +0100

    Got gamma band measure back for fitting in bash_utils.py

--- rising_net/scripts/bash_utils.py
+++ rising_net/scripts/bash_utils.py
@@ -60,27 +60,31 @@ def fit_rest(jobarr_id):
 
 def fit_task(jobarr_id):
     from rising_net.scripts.run_fit_plot import GSTR, RESSTR, NSDSTR
-    from rising_net.scripts.task_run_fit_plot import \
-        get_config, get_sim_res_COHM1S1diffratio_allbands, target_COHM1S1diffratio_allbands_fun, infer_nRuns_for_task
+    from rising_net.scripts.task_run_fit_plot import get_config, infer_nRuns_for_task, \
+        get_sim_res_COHM1S1diffratio_allbands, target_COHM1S1diffratio_allbands_fun, \
+        get_sim_res_COHM1S1diffratio_gamma, target_COHM1S1diffratio_gamma_fun
 
     # Fitting:
     config = get_config(FUNCMODE="FIT", BASENAME="FIT_TASK")[0]
 
     if config.COHERENCE_FISHER_Z_TRANSFORM:
         measure_labels = [
-                          "M1S1R_ThCOHFisherZdiffratio", "M1S1L_ThCOHFisherZdiffratio",
-                          "M1S1R_BtCOHFisherZdiffratio", "M1S1L_BtCOHFisherZdiffratio",
+                          # "M1S1R_ThCOHFisherZdiffratio", "M1S1L_ThCOHFisherZdiffratio",
+                          # "M1S1R_BtCOHFisherZdiffratio", "M1S1L_BtCOHFisherZdiffratio",
                           "M1S1R_GmCOHFisherZdiffratio", "M1S1L_GmCOHFisherZdiffratio"]
     else:
         measure_labels = [
-                          "M1S1R_ThCOHdiffratio", "M1S1L_ThCOHdiffratio",
-                          "M1S1R_BtCOHdiffratio", "M1S1L_BtCOHdiffratio",
+                          # "M1S1R_ThCOHdiffratio", "M1S1L_ThCOHdiffratio",
+                          # "M1S1R_BtCOHdiffratio", "M1S1L_BtCOHdiffratio",
                           "M1S1R_GmCOHdiffratio", "M1S1L_GmCOHdiffratio"]
 
     return infer_nRuns_for_task(iG=int(jobarr_id),
                                 priors=None, train_params_samples=None,
-                                sim_res=None, sim_res_path=None, sim_res_fun=get_sim_res_COHM1S1diffratio_allbands,
-                                target=None, target_fun=target_COHM1S1diffratio_allbands_fun, ground_truth=None,
+                                sim_res=None, sim_res_path=None,
+                                sim_res_fun=get_sim_res_COHM1S1diffratio_gamma,  # get_sim_res_COHM1S1diffratio_allbands,
+                                target=None,
+                                target_fun=target_COHM1S1diffratio_gamma_fun,  # target_COHM1S1diffratio_allbands_fun,
+                                ground_truth=None,
                                 config=config, folderstr=NSDSTR, resstr=RESSTR,
                                 label="", n_samples_per_run=None, measure_labels=measure_labels,
                                 save_samples=True, plot_flag=True, verbosity=2)
