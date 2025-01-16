dd26aae58ad5a4658011dc0f3af3bac8befda484
dionperd <dionperd@gmail.com>
2025-01-16 11:40:01 +0100

    Added sbatch_job_array_rest_ppp[0-5].sh

---
+++ rising_net/bash_scripts/sbatch_job_array_rest_ppc3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=FIT_REST_PPC3
+#SBATCH --output=./outputs/FIT_REST/PPC_sims/logs/FIT_REST_PPC3_%A_%a.out
+#SBATCH --error=./outputs/FIT_REST/PPC_sims/errors/FIT_REST_PPC3_%A_%a.err
+#SBATCH --array=0-5499
+#SBATCH --time=30:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --mail-user=dionysios.perdikis@bih-charite.de
+
+JOB_ID=$((${SLURM_ARRAY_TASK_ID} + 3*5500))
+CMD_PATH="/home/docker/packages/tvb-multiscale/rising_net/bash_scripts/"
+CMD="bash ${CMD_PATH}simulate_rest_ppc_jobarr.sh ${JOB_ID}"
+apptainer exec --bind $SCRATCH:$DOCKER_SCRATCH,$RISING_NET:$DOCKER_MULTISCALE --pwd $DOCKER_SCRATCH $RISING_NET_IMAGE $CMD
