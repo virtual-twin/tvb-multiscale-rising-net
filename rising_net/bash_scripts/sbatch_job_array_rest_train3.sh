351903e775801334fddcf2fefa3354dfcd219df4
dionperd <dionperd@gmail.com>
2025-01-16 11:34:53 +0100

    Removed the 100 jobs schedule from sbatch_job_array_rest_train*.sh

--- rising_net/bash_scripts/sbatch_job_array_rest_train3.sh
+++ rising_net/bash_scripts/sbatch_job_array_rest_train3.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=FIT_REST3
 #SBATCH --output=./outputs/FIT_REST/logs/FIT_REST3_%A_%a.out
 #SBATCH --error=./outputs/FIT_REST/errors/FIT_REST3_%A_%a.err
-#SBATCH --array=0-5499%100
+#SBATCH --array=0-5499
 #SBATCH --time=30:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
