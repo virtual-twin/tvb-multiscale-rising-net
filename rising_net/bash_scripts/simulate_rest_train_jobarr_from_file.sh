10345049ecbd3543ffa283f3452efffbc46558c8
dionperd <dionperd@gmail.com>
2025-01-17 17:44:32 +0100

    Minor correction in simulate_rest_train_jobarr_from_file.sh

--- rising_net/bash_scripts/simulate_rest_train_jobarr_from_file.sh
+++ rising_net/bash_scripts/simulate_rest_train_jobarr_from_file.sh
@@ -6,7 +6,6 @@ CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
 CMD_SCRIPT="rest_run_fit_plot.py"
 CMD_SCRIPTID_PATH='/home/docker/scratch'
 CMD_SCRIPTID_FILE="jobarray.txt"
-CMDID="simulate_rest_train_ids_args"
 DEF_ARGS="--FUNCMODE TRAINSIM --MODE TVB --BASENAME FIT_REST"
 
 $DOCKER_PYTHONPATH_CMD
