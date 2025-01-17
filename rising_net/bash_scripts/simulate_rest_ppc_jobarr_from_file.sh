33e5d445d6b021d1219a44914bd649840bc50dec
dionperd <dionperd@gmail.com>
2025-01-17 17:45:01 +0100

    Added simulate_rest_ppc_jobarr_from_file.sh & sbatch_job_array_rest_ppc_from_file.sh

---
+++ rising_net/bash_scripts/simulate_rest_ppc_jobarr_from_file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+JOBARRID=$1
+CMD="python"
+CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
+CMD_SCRIPT="rest_run_fit_plot.py"
+CMD_SCRIPTID_PATH='/home/docker/scratch'
+CMD_SCRIPTID_FILE="jobarray.txt"
+DEF_ARGS="--FUNCMODE PPCSIM --MODE TVB --BASENAME FIT_REST --fitlabel allsamples"
+
+$DOCKER_PYTHONPATH_CMD
+
+ARGSIDS=$(sed -n $(($JOBARRID+1))p $CMD_SCRIPTID_PATH/$CMD_SCRIPTID_FILE)
+echo ${ARGSIDS}
+
+CMD_ARGS="${DEF_ARGS} ${ARGSIDS}"
+echo ${CMD_ARGS}
+
+CMD="$CMD $CMD_PATH/$CMD_SCRIPT ${CMD_ARGS}"
+echo ${CMD}
+
+${CMD}
