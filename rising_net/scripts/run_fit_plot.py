c4653c26a670a08b6fc196c2519f50ec74c6255e
dionperd <dionperd@gmail.com>
2025-01-18 13:06:55 +0100

    Corrected bug in find_all_folders in run_fit_plot.py

--- rising_net/scripts/run_fit_plot.py
+++ rising_net/scripts/run_fit_plot.py
@@ -345,7 +345,7 @@ def find_all_folders(path, folderstr):
     ii = []
     for p in ensure_list(np.sort(glob.glob(pathstr))):
         ii.append(int(p[:-1].split("%s_" % folderstr)[-1]))
-    if len(ii):
+    if len(ii) == 0:
         warnings.warn("\nNo %s folderstr folders found in path\n%s\n!" % (folderstr, pathstr))
     return ii
 
