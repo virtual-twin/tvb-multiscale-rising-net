9028468e91e162ecbde39000a64b63a1b99eb866
dionperd <dionperd@gmail.com>
2025-01-31 22:34:03 +0100

    Introduced MAX_GAIN and new M1FACIAL_GAIN and CNM1S1_GAIN factors priors in base.py

--- rising_net/scripts/base.py
+++ rising_net/scripts/base.py
@@ -236,7 +236,7 @@ def configure(**ARGS):
         config.w_TVB_to_NEST["mossy_fibers"] = config.w_TVB_to_NEST_rest
     config.INVERSE_SIGMOIDAL_NEST_TO_TVB = True
 
-    config.PATHWAY_GAIN_WEIGHTS = {"M1FACIAL_GAIN": 1.2, "CNM1S1_GAIN": 0.5}
+    config.MAX_GAIN = 99.0
     # Fitting
     config.PRIORS_DEF = \
         {"I_s": {"prior_dist": "normal", "min": -0.25, "max": 0.45, "loc": 0.1, "sc": 0.1},
@@ -248,8 +248,8 @@ def configure(**ARGS):
          "I_w": {"prior_dist": "uniform", "min": -0.7, "max": 0.0, "loc": -0.35, "sc": 0.1},
          "G_w": {"prior_dist": "uniform", "min": 1.0, "max": 9.0, "loc": 5.0, "sc": 1.0},
          "PATHWAY_GAIN": {"prior_dist": "uniform", "min": 30.0, "max": 90.0, "loc": 60.0, "sc": 5.0},
-         # "M1FACIAL_GAIN": {"prior_dist": "uniform", "min": 10.0, "max": 99.0, "loc": 75.0, "sc": 5.0},
-         # "CNM1S1_GAIN": {"prior_dist": "uniform", "min": 1.0, "max": 99.0, "loc": 30.0, "sc": 10.0},
+         "PATHWAY_GAIN": {"prior_dist": "uniform", "min": 30.0, "max": 90.0, "loc": 60.0, "sc": 5.0},
+         "M1FACIAL_GAIN": {"prior_dist": "uniform", "min": 0.0, "max": 1.0, "loc": 0.5, "sc": 0.15},
          # "WHISKERS_GAIN": {"prior_dist": "uniform", "min": 1.0, "max": 90.0, "loc": 50.0, "sc": 10.0},
          # "TRIG_GAIN": {"prior_dist": "uniform", "min": 1.0, "max": 90.0, "loc": 50.0, "sc": 10.0},
          # "MEDULLA_GAIN": {"prior_dist": "uniform", "min": 1.0, "max": 90.0, "loc": 50.0, "sc": 10.0},
