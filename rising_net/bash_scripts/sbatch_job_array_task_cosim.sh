1089176fe76b801d507dffbd7785d22192027d0b
dionperd <dionperd@gmail.com>
2025-03-24 18:52:11 +0100

    Added simulate_task_cosim_w_fit_jobarr.sh and sbatch_job_array_task_cosim.sh

---
+++ rising_net/bash_scripts/sbatch_job_array_task_cosim.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=FTcosimW
+#SBATCH --output=./outputs/wTVBtoNESTfit/logs/FTcosimW_%A_%a.out
+#SBATCH --error=./outputs/wTVBtoNESTfit/errors/FTcosimW_%A_%a.err
+#SBATCH --array=0-104
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --mail-user=dionysios.perdikis@bih-charite.de
+
+JOB_ID=$((${SLURM_ARRAY_TASK_ID}))
+CMD_PATH="/home/docker/packages/tvb-multiscale/rising_net/bash_scripts/"
+CMD="bash ${CMD_PATH}simulate_task_cosim_w_fit_jobarr.sh ${JOB_ID}"
+apptainer exec --bind $SCRATCH:$DOCKER_SCRATCH,$RISING_NET:$DOCKER_MULTISCALE --pwd $DOCKER_SCRATCH $RISING_NET_IMAGE $CMD
---
+++ rising_net/bash_scripts/simulate_task_cosim_w_fit_jobarr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+JOBARRID=$1
+CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
+CMD_SCRIPT="bash_utils.py"
+CMDFUN="simulate_task_cosim_w_fit"
+
+$DOCKER_PYTHONPATH_CMD
+
+CMD="python $CMD $CMD_PATH/$CMD_SCRIPT $CMDFUN $JOBARRID"
+echo ${CMD}
+${CMD}
