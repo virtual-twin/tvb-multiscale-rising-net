1e64df9f36949ba1d3206f5a5f2beae965abe479
dionperd <dionperd@gmail.com>
2025-04-08 00:25:52 +0200

    Removed Inverse Sigmoidal transformer in thalamocortical_wc.py

--- tvb_multiscale/core/interfaces/transformers/models/thalamocortical_wc.py
+++ tvb_multiscale/core/interfaces/transformers/models/thalamocortical_wc.py
@@ -8,8 +8,9 @@ from tvb.basic.neotraits.api import HasTraits
 from tvb.basic.neotraits._attr import NArray
 
 from tvb_multiscale.core.interfaces.transformers.models.base import SpikesToRates, LinearRate
-from tvb_multiscale.core.interfaces.transformers.models.elephant import \
-    ElephantSpikesHistogram, ElephantSpikesHistogramRate, ElephantSpikesRate
+from tvb_multiscale.core.interfaces.transformers.models.integration import \
+    ElephantSpikesHistogramRateLinearIntegration, ElephantSpikesRateLinearIntegration, \
+    ElephantSpikesHistogramLinearIntegration,  ElephantSpikesHistogramRateLinearIntegration
 
 
 class ThalamocorticalWCLinearRate(LinearRate):
@@ -24,83 +25,6 @@ class ThalamocorticalWCLinearRate(LinearRate):
         return super(ThalamocorticalWCLinearRate, self)._compute(input_buffer.sum(axis=-1))
 
 
-class ThalamoCorticalWCInverseSigmoidal(HasTraits):
-
-    Rmin = NArray(
-        label=r":math:`R_min`",
-        default=np.array([0.5 * 1e-4, ]),
-        doc="""[Hz]. Minimum rate.""")
-
-    Rmax = NArray(
-        label=r":math:`R_max`",
-        default=np.array([1.0 - 1e-9, ]),
-        doc="""[Hz]. Maximum rate.""")
-
-    sigma = NArray(
-        label=r":math:sigma`",
-        default=np.array([0.0, ]),
-        doc="""[mV] Firing threshold (PSP) for which a 50% firing rate is achieved.
-                In other words, it is the value of the average membrane potential
-                corresponding to the inflection point of the sigmoid.
-                The usual value for this parameter is around 6.0.""")
-
-    beta = NArray(
-        label=r":math:`\beta`",
-        default=np.array([20.0, ]),
-        doc="""Steepness of the sigmoidal transformation.""")
-
-    w = NArray(
-        label=r":math:`w`",
-        default=np.array([1.0, ]),
-        doc="""Rate weight scaling.""")
-
-    def _compute(self, input_buffer):
-        return self.sigma - np.log(1.0 /
-                                   np.minimum(self.Rmax, np.maximum(self.w * input_buffer, self.Rmin)) - 1) / self.beta
-
-
-class ElephantSpikesHistogramThalamoCorticalWCInverseSigmoidal(ElephantSpikesHistogram,
-                                                               ThalamoCorticalWCInverseSigmoidal):
-
-    def configure(self):
-        ElephantSpikesHistogram.configure(self)
-        ThalamoCorticalWCInverseSigmoidal.configure(self)
-
-    def _compute(self, input_buffer):
-        """Method for the computation on the input buffer spikes' trains' data
-           for the output buffer data of instantaneous mean spiking rates to result."""
-        return ThalamoCorticalWCInverseSigmoidal._compute(
-            self, ElephantSpikesHistogram._compute(self, input_buffer))
-
-
-class ElephantSpikesHistogramRateThalamoCorticalWCInverseSigmoidal(ElephantSpikesHistogramRate,
-                                                                   ThalamoCorticalWCInverseSigmoidal):
-
-    def configure(self):
-        ElephantSpikesHistogramRate.configure(self)
-        ThalamoCorticalWCInverseSigmoidal.configure(self)
-
-    def _compute(self, input_buffer):
-        """Method for the computation on the input buffer spikes' trains' data
-           for the output buffer data of instantaneous mean spiking rates to result."""
-        return ThalamoCorticalWCInverseSigmoidal._compute(
-            self, ElephantSpikesHistogramRate._compute(self, input_buffer))
-
-
-class ElephantSpikesRateThalamoCorticalWCInverseSigmoidal(ElephantSpikesRate,
-                                                          ThalamoCorticalWCInverseSigmoidal):
-
-    def configure(self):
-        ElephantSpikesRate.configure(self)
-        ThalamoCorticalWCInverseSigmoidal.configure(self)
-
-    def _compute(self, input_buffer):
-        """Method for the computation on the input buffer spikes' trains' data
-           for the output buffer data of instantaneous mean spiking rates to result."""
-        return ThalamoCorticalWCInverseSigmoidal._compute(
-            self, ElephantSpikesRate._compute(self, input_buffer))
-
-
 class DefaultTVBtoSpikeNetTransformersThalamoCorticalWC(Enum):
     RATE = ThalamocorticalWCLinearRate
     # TODO: Need to potentially adjust all other transformers as well for summing up rates from all regions first!:
@@ -110,8 +34,8 @@ class DefaultTVBtoSpikeNetTransformersThalamoCorticalWC(Enum):
     # CURRENT = LinearCurrent
 
 
-class DefaultSpikeNetToTVBTransformersThalamoCorticalWCInverseSigmoidal(Enum):
-    SPIKES = ElephantSpikesHistogramRateThalamoCorticalWCInverseSigmoidal
-    SPIKES_TO_RATE = ElephantSpikesRateThalamoCorticalWCInverseSigmoidal
-    SPIKES_TO_HIST = ElephantSpikesHistogramThalamoCorticalWCInverseSigmoidal
-    SPIKES_TO_HIST_RATE = ElephantSpikesHistogramRateThalamoCorticalWCInverseSigmoidal
+class DefaultSpikeNetToTVBTransformersThalamoCorticalWC(Enum):
+    SPIKES = ElephantSpikesHistogramRateLinearIntegration
+    SPIKES_TO_RATE = ElephantSpikesRateLinearIntegration
+    SPIKES_TO_HIST = ElephantSpikesHistogramLinearIntegration
+    SPIKES_TO_HIST_RATE = ElephantSpikesHistogramRateLinearIntegration
