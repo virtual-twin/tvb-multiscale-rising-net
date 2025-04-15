31f125df4d2a0eecbe294757228b0ebac04aa0fc
dionperd <dionperd@gmail.com>
2025-04-15 17:34:40 +0200

    Minor correction in sbatch_job_array_cosim_CEREBON_OFF.sh

--- rising_net/bash_scripts/sbatch_job_array_cosim_CEREBON_OFF.sh
+++ rising_net/bash_scripts/sbatch_job_array_cosim_CEREBON_OFF.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=Cosim
-#SBATCH --output=./outputs/COSIM_CEREBON_OFF/logs/FTcosim_%A_%a.out
-#SBATCH --error=./outputs/COSIM_CEREBON_OFF/errors/FTcosim_%A_%a.err
+#SBATCH --output=./outputs/COSIM_CEREBON_OFF/logs/Cosim_%A_%a.out
+#SBATCH --error=./outputs/COSIM_CEREBON_OFF/errors/Cosim_%A_%a.err
 #SBATCH --array=0-59  # 99
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=1
