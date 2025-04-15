942e968f50e77a46ae04e684cde0c83f3b12f968
dionperd <dionperd@gmail.com>
2025-04-15 18:31:28 +0200

    Adjusted bash_utils.py and sbatch_job_array_cosim_CEREBON_OFF.sh for only CEREBOFF COSIM temporarily

--- rising_net/bash_scripts/sbatch_job_array_cosim_CEREBON_OFF.sh
+++ rising_net/bash_scripts/sbatch_job_array_cosim_CEREBON_OFF.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=Cosim
 #SBATCH --output=./outputs/COSIM_CEREBON_OFF/logs/Cosim_%A_%a.out
 #SBATCH --error=./outputs/COSIM_CEREBON_OFF/errors/Cosim_%A_%a.err
-#SBATCH --array=0-59  # 99
+#SBATCH --array=0-29  # 59  # 99
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=16G
