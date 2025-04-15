a002e975fc360f05299b4bf3ad8f6142136ab65e
dionperd <dionperd@gmail.com>
2025-04-15 17:35:06 +0200

    Added sbatch_job_array_tvb_CEREBON_OFF.sh & simulate_tvb_CEREBON_OFF_jobarr.sh

---
+++ rising_net/bash_scripts/simulate_tvb_CEREBON_OFF_jobarr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+JOBARRID=$1
+CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
+CMD_SCRIPT="bash_utils.py"
+CMDFUN="simulate_tvb_CEREBON_OFF"
+
+$DOCKER_PYTHONPATH_CMD
+
+CMD="python $CMD_PATH/$CMD_SCRIPT $CMDFUN $JOBARRID"
+echo ${CMD}
+${CMD}
