3a92b3fb10eabc393806a87966738997f1caa0ec
dionperd <dionperd@gmail.com>
2025-04-24 12:04:16 +0300

    Prepared sbatch_job_array_task_train_from_file.sh for FIT_TASKn4

--- rising_net/bash_scripts/sbatch_job_array_task_train_from_file.sh
+++ rising_net/bash_scripts/sbatch_job_array_task_train_from_file.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=FTtrain
-#SBATCH --output=./outputs/FIT_TASK/logs/FIT_TASK_%A_%a.out
-#SBATCH --error=./outputs/FIT_TASK/errors/FIT_TASK_%A_%a.err
-#SBATCH --array=1-5
-#SBATCH --time=06:00:00
+#SBATCH --job-name=FTtrainN4
+#SBATCH --output=./outputs/FIT_TASKn4/logs/FIT_TASK_%A_%a.out
+#SBATCH --error=./outputs/FIT_TASKn4/errors/FIT_TASK_%A_%a.err
+#SBATCH --array=1-214
+#SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
 #SBATCH --mail-user=dionysios.perdikis@bih-charite.de
@@ -12,4 +12,4 @@
 JOB_ID=$((${SLURM_ARRAY_TASK_ID}))
 CMD_PATH="/home/docker/packages/tvb-multiscale/rising_net/bash_scripts/"
 CMD="bash ${CMD_PATH}simulate_task_train_jobarr_from_file.sh ${JOB_ID}"
-apptainer exec --bind $SCRATCH:$DOCKER_SCRATCH,$RISING_NET:$DOCKER_MULTISCALE --pwd $DOCKER_SCRATCH $RISING_NET_IMAGE $CMD
+apptainer exec --bind $SCRATCH:$DOCKER_SCRATCH,$RISING_NET:$DOCKER_MULTISCALE --pwd $DOCKER_SCRATCH $RISING_NET_SANDBOX $CMD
