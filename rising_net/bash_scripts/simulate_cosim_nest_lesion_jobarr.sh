a9814a22854f6045f2361aca07dd4a7b383497d2
dionperd <dionperd@gmail.com>
2025-05-16 23:43:16 +0200

    Corrected simulate_cosim_nest_lesion_jobarr.sh

--- rising_net/bash_scripts/simulate_cosim_nest_lesion_jobarr.sh
+++ rising_net/bash_scripts/simulate_cosim_nest_lesion_jobarr.sh
@@ -3,7 +3,7 @@
 JOBARRID=$1
 CMD_PATH='/home/docker/packages/tvb-multiscale/rising_net/scripts'
 CMD_SCRIPT="bash_utils.py"
-CMDFUN="simulate_cosim_lesion_nest"
+CMDFUN="simulate_cosim_nest_lesion"
 
 $DOCKER_PYTHONPATH_CMD
 
