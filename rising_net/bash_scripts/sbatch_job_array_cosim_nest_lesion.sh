fcf56cfaf1be8728d7f4c5848d5f4d80bb1f6f94
dionperd <dionperd@gmail.com>
2025-06-10 19:44:46 +0200

    Corrected bug in sbatch_job_array_cosim_nest_lesion.sh

--- rising_net/bash_scripts/sbatch_job_array_cosim_nest_lesion.sh
+++ rising_net/bash_scripts/sbatch_job_array_cosim_nest_lesion.sh
@@ -5,7 +5,8 @@
 #SBATCH --error=./outputs/COSIM_NEST_LESION/errors/CosNESTles_%A_%a.err
 #SBATCH --array=0-29  # 69  # 0-79
 #SBATCH --time=24:00:00
-#SBATCH --cpus-per-task=1#SBATCH --mem-per-cpu=16G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=16G
 #SBATCH --mail-user=dionysios.perdikis@bih-charite.de
 
 JOB_ID=$((${SLURM_ARRAY_TASK_ID}))
