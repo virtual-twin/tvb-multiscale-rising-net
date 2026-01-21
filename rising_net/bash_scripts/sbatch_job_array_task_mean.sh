057b0e41391b754c9c5ce689670fddc186ac5fd2
dionperd <dionperd@gmail.com>
2026-01-21 20:33:25 +0100

    Corrected folder name in sbatch_job_array_task_mean.sh

--- rising_net/bash_scripts/sbatch_job_array_task_mean.sh
+++ rising_net/bash_scripts/sbatch_job_array_task_mean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=FTmn
-#SBATCH --output=./outputs/FIT_TASKn4/mean_sims/allsamples/logs/FIT_TASK_MEAN_%A_%a.out
-#SBATCH --error=./outputs/FIT_TASKn4/mean_sims/allsamples/errors/FIT_TASK_MEAN_%A_%a.err
+#SBATCH --output=./outputs/FIT_TASK/mean_sims/allsamples/logs/FIT_TASK_MEAN_%A_%a.out
+#SBATCH --error=./outputs/FIT_TASK/mean_sims/allsamples/errors/FIT_TASK_MEAN_%A_%a.err
 #SBATCH --array=0-49
 #SBATCH --time=06:00:00
 #SBATCH --cpus-per-task=1
